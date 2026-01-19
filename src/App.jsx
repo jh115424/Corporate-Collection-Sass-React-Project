bb7181bbe2dbbee37920342c8940ea443647768c
jennifer hodge <jennifer.hodge@russdarrow.com>
2026-01-19 17:56:33 -0600

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,11 @@ import StatsSection from "./Components/StatsSection";
 import FurnitureSlab from "./Components/FurnitureSlab";
 import Footer from "./Components/Footer";
 
-// function App() {
-//   return (
-//     <>
-//       <TopHeader />
-//       <SubHeader />
-//       <LandingStatement />
-//       <LandingPageTopImage />
-//       <CompanyCarousel />
-//       <StatsSection />
-//       <FurnitureSlab />
-//       <Footer />
-//       <p className="disclaimer">
-//         Portfolio demonstration. Logos used for presentation purposes only."
-//       </p>
-//     </>
-//   );
-// }
+
 
 function App() {
   return (
-    <div style={{position: 'relative', zIndex: 10, backgroundColor: 'white', paddingBottom: '100vh'}}>
+    <div style={{position: 'relative', zIndex: 10, backgroundColor: 'white'}}>
       <TopHeader />
       <SubHeader />
       <LandingStatement />
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,27 +1,27 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 import "./footer.css";
-import { useRef } from "react";
+// import { useRef } from "react";
 
 export default function Footer() {
-  const pullDownName = useRef(null);
+//   const pullDownName = useRef(null);
 
-  useEffect(() => {
-    let wordObserve = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          console.log("Name is in view");
+//   useEffect(() => {
+//     let wordObserve = new IntersectionObserver((entries) => {
+//       entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//           console.log("Name is in view");
 
-          pullDownName.current.classList.add("ending-active");
-        }
-      });
-    });
+//           pullDownName.current.classList.add("ending-active");
+//         }
+//       });
+//     });
 
-    wordObserve.observe(pullDownName.current);
+//     wordObserve.observe(pullDownName.current);
 
-    return () => {
-      wordObserve.disconnect();
-    };
-  }, []);
+//     return () => {
+//       wordObserve.disconnect();
+//     };
+//   }, []);
 
   return (
     <>
@@ -29,9 +29,10 @@ export default function Footer() {
         <p className="firstFooterColumn">Company</p>
         <p className="secondFooterColumn">Resources</p>
         <p className="thirdFooterColumn">Support</p>
+
       </div>
       <div className="companyNameFinisher">
-        <p ref={pullDownName} className="ending">
+        <p className="ending">
           Corporate Collection
         </p>
       </div>
